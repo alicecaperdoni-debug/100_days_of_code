e720053675a0ed7072dd59f8ad62848dde767bb7
Alice <alice.caperdoni@gmail.com>
2026-04-06 16:54:01 +0200

    uncommented format_name

--- Day 10/Functions with Outputs/task.py
+++ Day 10/Functions with Outputs/task.py
@@ -1,9 +1,9 @@
-# def format_name(f_name, l_name):
-#     formated_f_name = f_name.title()
-#     formated_l_name = l_name.title()
-#     return f"{formated_f_name} {formated_l_name}"
-#
-# print(format_name("alIcE", "CAPE"))
+def format_name(f_name, l_name):
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+print(format_name("alIcE", "CAPE"))
 
 def function_1(text):
     return text + text
