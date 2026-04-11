a68cb76668964ec0a9520bcf0f5812f67e49751c
Alice <alice.caperdoni@gmail.com>
2026-04-11 10:57:00 +0200

    the end?

--- Day 12/Number Guessing Project/main.py
+++ Day 12/Number Guessing Project/main.py
@@ -1,15 +1,8 @@
 import random
-
-def make_guess(): # due funzioni, una make_guess ed una check_guess?
-    user_guess = int(input("Make a guess: ")) # questo probabilmente va fuori; poi "Guess again:"
-    if user_guess == COMPUTER_CHOICE:
-        print(f"You got it! The answer was {COMPUTER_CHOICE}")
-    elif user_guess > COMPUTER_CHOICE:
-        print("Too high!\nGuess again.")
-    elif user_guess < COMPUTER_CHOICE:
-        print("Too low!\nGuess again.")
+import art
 
 COMPUTER_CHOICE = random.randrange(1, 101)
+print(art.logo)
 print("Welcome to the Number Guessing Game!")
 print ("I'm thinking of a number between 1 and 100.")
 difficulty = input("Choose a difficulty. Type \"easy\" or \"hard\":\n").lower()
@@ -19,7 +12,21 @@ if difficulty == "easy":
 elif difficulty == "hard":
     attempts = 5
 
-for i in range(attempts): # fermare il loop se la risposta è stata trovata
+game_continue = True
+
+while game_continue and attempts > 0:
     print(f"You have {attempts} attempts remaining to guess the number.")
-    make_guess()
+    user_guess = int(input("Make a guess: "))
+    if user_guess == COMPUTER_CHOICE:
+        print(f"You got it! The answer was {COMPUTER_CHOICE}")
+        game_continue = False
+    elif user_guess > COMPUTER_CHOICE:
+        print("Too high!")
+    elif user_guess < COMPUTER_CHOICE:
+        print("Too low!")
     attempts -= 1
+    if attempts > 0:
+        print("Guess again.")
+
+if attempts == 0:
+    print("You've run out of guesses.")
